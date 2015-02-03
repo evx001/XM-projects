fec06e0956fd1b9c6ac98b267c994fae47e939de
evx001 <eviexeris@gmail.com>
2015-02-03 13:56:54 -0500

    works

---
+++ Consoling000/Consoling000/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module Consoling000.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("Consoling000")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("evx001")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
