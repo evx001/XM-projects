32fb23788190fb23f1d9a21bea801682e74d309f
evx001 <eviexeris@gmail.com>
2015-02-16 23:09:59 -0500

    corrected

--- elosnoc000/elosnoc000/Program.fs
+++ elosnoc000/elosnoc000/Program.fs
@@ -1,7 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
-
+module myList1 
 let xs = [1;2;3]
 let ys = [4;5;6] 
 let xss = [11,13,17]
@@ -11,8 +11,16 @@ let zss = (xss @ yss)
 
 
 printfn "%A" (zs)
-printfn "%A" (zss)
+printfn "%A" (zss )
+printfn "%A" (0::xs)
 
+
+
+(*//> outputs 
+[1; 2; 3; 4; 5; 6]
+[(11, 13, 17); (19, 23, 31)]
+[0; 1; 2; 3]
+*)
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
