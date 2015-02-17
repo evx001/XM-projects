00a0eb4f7e1633034a139ab6eef24de3e96c9119
evx001 <eviexeris@gmail.com>
2015-02-16 22:58:07 -0500

    lists

---
+++ elosnoc000/elosnoc000/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+
+let xs = [1;2;3]
+let ys = [4;5;6] 
+let xss = [11,13,17]
+let yss = [19,23,31]
+let zs = (xs @ ys)
+let zss = (xss @ yss)
+
+
+printfn "%A" (zs)
+printfn "%A" (zss)
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+
+    0 // return an integer exit code
