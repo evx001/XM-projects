8712606145642a6406db7459abb62336c003e6dc
evx001 <eviexeris@gmail.com>
2015-02-16 22:57:00 -0500

    consoling updated

--- Consoling000/Consoling000/Program.fs
+++ Consoling000/Consoling000/Program.fs
@@ -1,8 +1,78 @@
-﻿/// works !
+﻿// namespace global
 module TheBookOfFSharp.RpnCalculator
-
 open System
 
+let square x = x * x 
+
+let add x y = 
+    x + y 
+//let add' x y = 
+//    let result =
+//         x + y 
+
+let add5and3 = add 5 3 
+
+let result = add (square 12) 7   
+
+[<EntryPoint>]         
+printfn "square 5 is %i" result    
+
+
+(* 
+Xunit is good testing frame work for functions. 
+*) 
+
+
+
+ 
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// open System.Windows.Forms 
+
+(*
+type Person = {Name: string; Age: int} 
+    let testData = 
+                [| 
+                {Name = "Harry"; Age = 37}; 
+                {Name = "July"; Age = 41}; 
+                |] 
+        let form = new Form(Text = "F# Windows Form")
+
+            let dataGrid = new DataGridView(Docl=DockStyle.Fill, DataSource = testData)
+*) 
+ /// form.Controls.Add(dataGrid)
+
+ // Application.Run(form) 
+
+
+(* ************************************
 let evalRpnExpr (s : string) =
   let solve items current =
     match (current, items) with
@@ -25,3 +95,22 @@ let main argv =
   |> List.iter (fun (expr, result) -> printfn "(%s) = %A" expr result)
   Console.ReadLine() |> ignore
   0
+ ********************************************** *)
+ (* ***********************************************
+open system.Windows.Forms 
+type Person = {Name: string; Age: int} 
+
+let testData = 
+    [| 
+        {Name = "Harry"; Age = 37}; 
+        {Name = "July"; Age = 41}; 
+    |] 
+
+ let form = new Form(Text = "F# Windows Form")
+
+ let dataGrid = new DataGridView(Docl=DockStyle.Fill, DataSource = testData)
+ form.Controls.Add(dataGrid)
+
+ Application.Run(form) 
+/// ********************************************** *)
+/// [<EntryPoint>]
---
+++ Consoling000/Consoling000/helloForms.fs
@@ -0,0 +1,30 @@
+﻿module TheBookOfFSharp.RpnCalculator
+/// Hello Forms 
+
+
+
+(*
+printfn "Hi" 
+
+let response = System.Console.ReadLine() 
+
+printfn "Great to hear from you %A " response 
+
+
+open system.Windows.Forms 
+type Person = {Name: string; Age: int} 
+
+let testData = 
+    [| 
+        {Name = "Harry"; Age = 37}; 
+        {Name = "July"; Age = 41}; 
+    |] 
+
+ let form = new Form(Text = "F# Windows Form")
+
+ let dataGrid = new DataGridView(Docl=DockStyle.Fill, DataSource = testData)
+ form.Controls.Add(dataGrid)
+
+ Application.Run(form) 
+*)
+ 
