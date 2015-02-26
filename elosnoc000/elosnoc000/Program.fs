924e107771d1a04f07e198e31c3e51afd6214af3
evx001 <eviexeris@gmail.com>
2015-02-26 00:46:35 -0500

    empty prog loads

--- elosnoc000/elosnoc000/Program.fs
+++ elosnoc000/elosnoc000/Program.fs
@@ -1,15 +1,11 @@
-﻿// functions 
-let ys = [1;0;2]
-let rec listLength  = function 
-    | [] -> 0 
-    | _ :: xs -> 1 + ( listLength xs)
+﻿namespace elosnoc000
 
 
-printfn "%A" (listLength ys)
-
+(*
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
 
 
     0 // return an integer exit code
+*)
