2032b8601c3ed9971b138ed019bdd2a8366bce0e
evx001 <eviexeris@gmail.com>
2015-03-01 22:53:59 -0500

    program change

--- elosnoc000/elosnoc000/Program.fs
+++ elosnoc000/elosnoc000/Program.fs
@@ -1,14 +1,25 @@
 ﻿
 // open elosnoc000.modCsv
 
-open elosnoc000.yum001
-printfn "%A" xs  
+module AgeTest =
+    type Person = { Name: string; Age: int}
 
+    let testData = 
+        [| 
+            {Name = "Joe"; Age = 37};
+            {Name = "Julie"; Age = 35}
+        |] 
 
+(*
+let form = new Form (Text = "F# Win Form")
+let dataGrid = new DataGridView(Dock=DockStyle.Fill, DataSource = testData)
+form.Controls.Add(dataGrid)
+Application.Run(form)
+*)
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    let xo = System.Console.ReadLine()
+    let xXx = System.Console.ReadLine()
 
     0 // return an integer exit code
