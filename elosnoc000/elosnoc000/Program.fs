fda7b94debcfdd43b132da97635e33053a683be3
evx001 <eviexeris@gmail.com>
2015-03-03 00:23:22 -0500

    vs loads with refs

--- elosnoc000/elosnoc000/Program.fs
+++ elosnoc000/elosnoc000/Program.fs
@@ -1,4 +1,4 @@
-﻿
+﻿open System.Windows.Forms
 // open elosnoc000.modCsv
 
 module AgeTest =
@@ -10,12 +10,12 @@ module AgeTest =
             {Name = "Julie"; Age = 35}
         |] 
 
-(*
+
 let form = new Form (Text = "F# Win Form")
-let dataGrid = new DataGridView(Dock=DockStyle.Fill, DataSource = testData)
+let dataGrid = new DataGridView(Dock=DockStyle.Fill, DataSource = AgeTest.testData)
 form.Controls.Add(dataGrid)
 Application.Run(form)
-*)
+
 
 [<EntryPoint>]
 let main argv = 
