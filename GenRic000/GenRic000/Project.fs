83b4a5c65a6a1af38445ac3973db91073e439208
evx001 <eviexeris@gmail.com>
2015-03-02 19:37:39 -0500

    project fs entry point

--- GenRic000/GenRic000/Project.fs
+++ GenRic000/GenRic000/Project.fs
@@ -1,1 +1,10 @@
-﻿
+﻿namespace GenRic000
+
+
+module entry = 
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" argv
+        let xXx = System.Console.ReadLine()
+
+        0 // return an integer exit code
