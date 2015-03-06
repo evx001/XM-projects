dfda5a0a9c89217054709103309e65de1c882926
evx001 <eviexeris@gmail.com>
2015-03-06 12:57:23 -0500

    as // unexpected keyword

--- elosnoc000/elosnoc000/Program.fs
+++ elosnoc000/elosnoc000/Program.fs
@@ -6,7 +6,7 @@ try
     use file000 = File.OpenText "FSharpMobileDev.txt"
     file000.ReadToEnd() |>  printfn "%A" 
  with 
- |    :? FileNotFoundException -> printfn "File not Found"
+ |    :? FileNotFoundException -> as ex -> printfn "% File not Found" ex.FileName
  |    _  -> printfn "Error loading file"
 
 [<EntryPoint>]
